ae116170e8d7b486313ab16659a34251541447e0
Nguyen Quang <Nguyenquanguet@gmail.com>
2021-06-04 13:42:08 +0700

    sum

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(){
-    println("Hello World !");
+    printf("Hello World !");
     return 0;
 }
---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a, b;
+    printf("Nhap so nguyen to a: ");
+    scanf("%d", &a);
+    printf("\nNhap so nguyen to b: ");
+    scanf("%d", &b);
+    printf("\nTong cua 2 so a va b la: %d", a +b);
+    return 0;
+}
